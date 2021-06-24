57916f634b368b6fc8a11582a137b3e91a47e9f5
Gabriel Gonçalves Pereira <ggonpereira@gmail.com>
2021-06-23 21:14:36 -0300

    Day 3: Making connection with DB. Ask room created

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,14 +4,17 @@ import illustrationImg from "../assets/images/illustration.svg";
 import logoImg from "../assets/images/logo.svg";
 import googleIconImg from "../assets/images/google-icon.svg";
 
+import { database } from "../services/firebase";
+
 import { Button } from "../components/Button";
 import { useAuth } from "../hooks/useAuth";
 
 import "../styles/auth.scss";
-
+import { FormEvent, useState } from "react";
 
 export function Home() {
   const history = useHistory();
+  const [roomCode, setRoomCode] = useState("");
   const { user, signInWithGoogle } = useAuth();
 
   async function handleCreateRoom() {
@@ -22,10 +25,28 @@ export function Home() {
     history.push("/rooms/new");
   }
 
+  async function handleJoinRoom(event: FormEvent) {
+    event.preventDefault();
+
+    if (roomCode.trim() === "") return;
+
+    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+    if (!roomRef.exists()) {
+      alert("Room does not exists.");
+      return;
+    }
+
+    history.push(`/rooms/${roomCode}`);
+  }
+
   return (
     <div id="page-auth">
       <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <img
+          src={illustrationImg}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
         <strong>Crie salas de Q&amp;A ao-vivo</strong>
         <p>Tire as dúvidas da sua audiência em tempo-real</p>
       </aside>
@@ -37,10 +58,12 @@ export function Home() {
             Crie sua sala com o Google
           </button>
           <div className="separator">ou entre em uma sala</div>
-          <form>
+          <form onSubmit={handleJoinRoom}>
             <input
               type="text"
               placeholder="Digite o código da sala"
+              onChange={(event) => setRoomCode(event.target.value)}
+              value={roomCode}
             />
             <Button type="submit">Entrar na sala</Button>
           </form>
